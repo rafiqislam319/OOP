90f380665baa8a03b6dab81ba61bdbf4f2b196c4
rafiqislam319 <rafiqislam319@gmail.com>
2022-05-15 00:36:07 +0600

    inheritance implementation

--- index.php
+++ index.php
@@ -1,21 +1,54 @@
 <?php
 
+    class User {
+        public $userName;
+        public $userEmail;
+        private $phoneNumber;   //private property
+
+        function __construct($name, $mail, $phone) {
+            $this->userName = $name;
+            $this->userEmail = $mail;
+            $this->phoneNumber = $phone;
+        }
+        function getPhone() {           //getter method to access private property
+            return $this->phoneNumber;
+        }
+        function setPhone($phone) {     //setter method to set value
+            $this->phoneNumber = $phone;
+        }
+
+    }
+        //child class
+    class AdminUser extends User {
+
+        public $level;
+
+        function __construct($name, $mail, $phone, $lvl) {
+            $this->level = $lvl;
+            parent:: __construct($name, $mail, $phone);       /* this will access the common property from parent class, 
+                                                                since a constructor function is already here but the common properties are not here, 
+                                                               to overcome tis issue we have to use this procedure.*/                                                                                                                     
+        }
+    }
+
+
+    $userOne = new User('Jon', 'jon@gmail.com', '0121');
+    $userTwo = new User('Tom', 'tom@gmail.com', '0141');
+    $userThree = new AdminUser('Jerry', 'jerry@gmail.com', '0000', 4);
+
+    $userOne->setPhone('111');  //for set phone
+
+    echo $userOne->userName .'<br>';
+    echo $userOne->userEmail .'<br>';
+    echo $userOne->getPhone() .'<br>';
+
+    echo $userTwo->userName .'<br>';
+    echo $userTwo->userEmail .'<br>';
+    echo $userTwo->getPhone() .'<br>';
+
+    echo $userThree->userName .'<br>';
+    echo $userThree->userEmail .'<br>';
+    echo $userThree->getPhone() .'<br>';
+    echo $userThree->level .'<br>';
 
 ?>
-
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>OOP PHP</title>
-</head>
-<body>
-    <?php 
-    
-    ?>
-</body>
-</html>
