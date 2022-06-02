80a6087216338c9154f1a533d2cf225bab16bd9a
rafiqislam319 <rafiqislam319@gmail.com>
2022-06-02 18:35:55 +0600

    protected modifier

--- index.php
+++ index.php
@@ -1,21 +1,39 @@
 <?php
 
+    class User {
+        public $userName;
+        protected $userEmail;   //protected property
+
+        function __construct($name, $mail) {
+            $this->userName = $name;
+            $this->userEmail = $mail;
+        }
+ 
+        public function message(){
+            return "$this->userEmail, send a message";
+        }
+
+        
+
+    }
+        //child class
+    class AdminUser extends User {
+
+        function __construct($name, $mail) {
+            parent::__construct($name, $mail);                                                               
+        }
+        public function message(){
+            return "$this->userEmail , An Admin, send a message";
+        }
+
+
+    }
+
+
+    $userOne = new User('Jon', 'jon@gmail.com');
+    $userTwo = new AdminUser('Tom', 'tom@gmail.com');
+
+
+    echo $userTwo->message() .'<br>';    // getting protected property from child class instance.
 
 ?>
-
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>OOP PHP</title>
-</head>
-<body>
-    <?php 
-    
-    ?>
-</body>
-</html>
