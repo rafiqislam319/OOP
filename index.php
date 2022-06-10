06180d72e92d354e256e7ced93a3460a79d55adc
rafiqislam319 <rafiqislam319@gmail.com>
2022-06-10 19:56:30 +0600

    destruct function

--- index.php
+++ index.php
@@ -1,21 +1,18 @@
 <?php
+class Fruit {
+  // Properties
+  var $name;
+  var $color;
 
+  // Methods
+  function __construct($name, $color) {
+    $this->name = $name;
+    $this->color = $color; 
+  }
+  function __destruct() {
+    echo "The fruit is {$this->name} and the color is {$this->color}."; 
+  }
+}
 
+$apple = new Fruit("Apple", "red");
 ?>
-
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>OOP PHP</title>
-</head>
-<body>
-    <?php 
-    
-    ?>
-</body>
-</html>
