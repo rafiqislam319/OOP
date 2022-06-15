8ab922b6333e1dc10b12a5002dfabaa55637e18f
rafiqislam319 <rafiqislam319@gmail.com>
2022-06-15 19:36:28 +0600

    constant keyword

--- index.php
+++ index.php
@@ -1,21 +1,17 @@
 <?php
 
+	class Circle {
+		const PI = 3.1416;
+
+		function getRadius($radius){
+
+			return $radius = $radius * self::PI;  // In class self:: is used to access constant
+		}
+	}
+
+
+	$circle = new Circle();
+	echo Circle::PI . "<br>";   // outside the class we can access contant directly with ClassName::
+	echo $circle->getRadius(5);
 
 ?>
-
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>OOP PHP</title>
-</head>
-<body>
-    <?php 
-    
-    ?>
-</body>
-</html>
